743529f9d092a2ec5fb2a224073f32dc645a1c8b
SDG1804 <s.diazgonzalez@mail.utoronto.ca>
2023-09-18 18:54:10 -0400

    "Small changes"

---
+++ src/ArrayListMultiSet.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListMultiSet {
+    public ArrayListMultiSet() {
+        List<Object> list = new ArrayList<>();
+    }
+
+    public boolean add(Object item){
+
+    }
+}
